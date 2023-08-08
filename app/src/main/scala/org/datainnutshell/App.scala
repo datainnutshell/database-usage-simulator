5d34d2de56ea616c17579bc1fa21d73f006ff52e
André Santos <asantoz@gmail.com>
2023-08-08 16:17:51 +0100

    Create docker-publish.yml

--- app/src/main/scala/org/datainnutshell/App.scala
+++ app/src/main/scala/org/datainnutshell/App.scala
@@ -11,43 +11,43 @@ import scala.concurrent.duration.{DurationInt}
 import scala.util.{Failure, Success}
 
 object App extends App {
-  implicit val system: ActorSystem                        = ActorSystem("ProductStockSimulation")
+  implicit val system: ActorSystem = ActorSystem("ProductStockSimulation")
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   // Read database configuration from environment variables
-  private val mysqlHost     = sys.env.getOrElse("MYSQL_HOST", "localhost")
-  private val mysqlPort     = sys.env.getOrElse("MYSQL_PORT", "3306").toInt
-  private val mysqlDb       = sys.env.getOrElse("MYSQL_DB", "product_stock_db")
-  private val mysqlUser     = sys.env.getOrElse("MYSQL_USER", "dbuser")
+  private val mysqlHost = sys.env.getOrElse("MYSQL_HOST", "localhost")
+  private val mysqlPort = sys.env.getOrElse("MYSQL_PORT", "3306").toInt
+  private val mysqlDb = sys.env.getOrElse("MYSQL_DB", "product_stock_db")
+  private val mysqlUser = sys.env.getOrElse("MYSQL_USER", "dbuser")
   private val mysqlPassword = sys.env.getOrElse("MYSQL_PASSWORD", "dbpassword")
 
   val dbUrl = s"jdbc:mysql://$mysqlHost:$mysqlPort/$mysqlDb"
-  val db    = Database.forURL(url = dbUrl, user = mysqlUser, password = mysqlPassword)
+  val db = Database.forURL(url = dbUrl, user = mysqlUser, password = mysqlPassword)
 
-  // Define the setup queries for product stock
+  // Define the database schema structure
   val setup = DBIO.seq(
     SimulatorSchemas.products.schema.createIfNotExists,
     SimulatorSchemas.productTransactions.schema.createIfNotExists
   )
 
-  //  Perform setup and then start the simulation
+  //  Perform setup and seed with initial products
   val setupAndStartFuture = db.run(setup andThen SimulatorSchemas.seedProducts)
 
   // Shutdown the system after a while (for demonstration purposes)
   setupAndStartFuture.onComplete {
     case Success(_) => {
       log.info("Database setup and seeding completed successfully.")
-      val simulator =
-        system.actorOf(ProductTransactionSimulator.props(db), "productTransactionSimulator")
+      system.actorOf(ProductTransactionSimulator.props(db), "productTransactionSimulator")
 
-      system.scheduler.scheduleOnce(10.minutes) {
+      system.scheduler.scheduleOnce(30.minutes) {
         system.terminate()
         db.close()
       }
     }
     case Failure(ex) => {
       log.error(ex, "Error during database setup and seeding.")
-      context.stop(self) // Stop the actor if setup fails
+      // Stop the actor if setup fails
+      context.stop(self)
     }
   }
 }
